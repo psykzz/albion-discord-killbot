c43c98684779040edc52a4fc3c95a3695db75604
Matt Smith <psykzz@users.noreply.github.com>
2017-07-20 21:54:14 +0100

    Update fame stats

--- src/lib/discord-plugins/player-info.js
+++ src/lib/discord-plugins/player-info.js
@@ -30,7 +30,7 @@ class PlayerInfo extends Plugin {
           albionAPI.getPlayerInfo(player.Id, cb);
         },
         function replyMessage(playerInfo, cb) {
-          message.reply(`\n\`\`\`text\nPlayer: ${playerInfo.Name}\nGuild: ${playerInfo.GuildName} - [${playerInfo.AllianceTag}] ${playerInfo.AllianceName}\nAvg Item Power: ${playerInfo.AverageItemPower}\n\n** Fame **\nKills: ${playerInfo.killFame}\nDeaths: ${playerInfo.DeathFame}\nRatio: ${playerInfo.killFame / playerInfo.DeathFame}\`\`\``);
+          message.reply(`\n\`\`\`text\nPlayer: ${playerInfo.Name}\nGuild: ${playerInfo.GuildName} - [${playerInfo.AllianceTag}] ${playerInfo.AllianceName}\nAvg Item Power: ${playerInfo.AverageItemPower}\n\n** Fame **\nKills: ${playerInfo.KillFame}\nDeaths: ${playerInfo.DeathFame}\nRatio: ${playerInfo.FameRatio}\`\`\``);
           cb();
         }
       ], (err) => {
