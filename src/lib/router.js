8b194dad79b80ed2d6d97c3bcbcfe7d606b4302c
Matt Smith <psykzz@users.noreply.github.com>
2017-09-11 20:03:24 +0100

    add guilds endpoint

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var bot = require('../app').bot;
+
 var config = require('../config');
 
 // define the home page route
 router.get('/', function (req, res) {
   res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${config.DISCORD_CLIENT_ID}&scope=bot&permissions=0`);
 });
+
+router.get('/guilds', function (req, res) {
+  res.json({
+    guilds: bot.guilds.map(item => {
+      return {
+        name: item.name,
+        id: item.id
+      }
+    })
+  })
+})
 
 module.exports = router;
