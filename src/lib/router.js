62c998d59ccf88e05b449c1ad5934ef4d0cee6d0
Matt Smith <psykzz@users.noreply.github.com>
2017-09-13 23:41:01 +0100

    Update router.js

--- src/lib/router.js
+++ src/lib/router.js
@@ -18,7 +18,7 @@ router.get('/guilds', function (req, res) {
     guilds: app.bot.client.guilds.map(item => {
       return {
         name: item.name,
-        owner: `${item.owner.user.username}#${item.owner.user.discriminator}`
+        owner: `${item.owner.user.username}#${item.owner.user.discriminator}`,
         id: item.id
       }
     })
